c8360271069e1f404852b1af1eb47ee22f7c27c9
zephyrus21 <pandey.piyus@gmail.com>
2024-06-22 02:27:57 +0530

    feat: completed grep logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,84 @@
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(config.filename)?;
-    println!("{}", content);
+    let contents = fs::read_to_string(config.filename)?;
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    let data = results.unwrap_or_else(|err| {
+        println!("Problem getting result: {}", err);
+        std::process::exit(1);
+    });
+
+    for line in data {
+        println!("{}", line);
+    }
+
     Ok(())
 }
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &str> {
-        let query: String = args[1].clone();
-        let filename: String = args[2].clone();
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
 
-        Ok(Config { query, filename })
+        let filename: String = args[1].clone();
+        let query: String = args[2].clone();
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Result<Vec<&'a str>, &'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    if results.len() < 1 {
+        return Err("no text found");
+    }
+
+    Ok(results)
+}
+
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Result<Vec<&'a str>, &'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    if results.len() < 1 {
+        return Err("no text found");
+    }
+
+    Ok(results)
 }
